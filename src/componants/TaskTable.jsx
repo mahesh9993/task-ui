dd8bf179247092db52a480902ce33441dbbb69ab
Mahesh Chathuranga <mahesh.chathuranga9993@gmail.com>
2024-10-08 19:50:56 +0530

    impliment filtering

--- src/componants/TaskTable.jsx
+++ src/componants/TaskTable.jsx
@@ -11,43 +11,66 @@ function TaskTable() {
   const [currentPage, setCurrentPage] = useState(1);
   const [paginatedTasks, setPaginatedTasks] = useState([]);
   const [users, setUsers] = useState([]);
-  const pageSize = 5;
+  const [selectedUser, setSelectedUser] = useState([]);
+  const pageSize = 10;
   const tableHeaders = ["Task Id", "User Id", "Title", "Status", ""];
 
   useEffect(() => {
-    getTasks().then((res) => setTasks(res.data.slice(0, 50)));
+    getTasks().then((res) => setTasks(res.data));
   }, []);
+
+  const filteredTasks =
+    selectedUser && selectedUser.id
+      ? tasks.filter((t) => t.userId === selectedUser.id)
+      : tasks;
 
   useEffect(() => {
     getUsers();
-    setPaginatedTasks(paginate(tasks, currentPage, pageSize));
-  }, [tasks, currentPage]);
+    setPaginatedTasks(paginate(filteredTasks, currentPage, pageSize));
+  }, [tasks, currentPage, selectedUser]);
 
   //console.log("tasks", tasks);
 
-  const handleDelete = (id) => {
+  function handleDelete(id) {
     const updatedTaskList = tasks.filter((t) => t.id !== id);
     setTasks(updatedTaskList);
     //console.log(tasks);
     deleteTask(id);
-  };
+  }
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
   function getUsers() {
-    const usersArray = [];
-    tasks.map((task) => usersArray.push(task.userId));
-    const users = _.uniq(usersArray);
+    const usersArray = [{ id: "", name: "All Users" }];
+    tasks.map((task) => {
+      const user = {};
+      user.id = task.userId;
+      user.name = "User " + task.userId;
+      usersArray.push(user);
+    });
+    const users = _.uniqBy(usersArray, "id");
     setUsers(users);
     //console.log(users);
   }
 
+  const handleUserSelect = (user) => {
+    //console.log(user);
+    setSelectedUser(user);
+    setCurrentPage(1);
+  };
+
   return (
     <div className="row">
       <div className="col-2">
-        <ListGroup items={users} />
+        <ListGroup
+          items={users}
+          valueProperty="id"
+          textProperty="name"
+          onItemSelect={handleUserSelect}
+          selectedItem={selectedUser}
+        />
       </div>
       <div className="col">
         <div className="container">
@@ -57,7 +80,7 @@ function TaskTable() {
             headers={tableHeaders}
           />
           <Pagination
-            itemsCount={tasks.length}
+            itemsCount={filteredTasks.length}
             pageSize={pageSize}
             onPageChange={handlePageChange}
             currentPage={currentPage}
--- src/componants/common/ListGroup.jsx
+++ src/componants/common/ListGroup.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-const ListGroup = () => {
-  return <div>ListGroup</div>;
+const ListGroup = ({
+  items,
+  onItemSelect,
+  selectedItem,
+  valueProperty,
+  textProperty,
+}) => {
+  return (
+    <div className="ms-5 mt-3">
+      <ul class="list-group">
+        {items.map((item) => (
+          <li
+            class={
+              item[valueProperty] === selectedItem[valueProperty]
+                ? "list-group-item cursor-pointer active"
+                : "list-group-item cursor-pointer"
+            }
+            key={item[valueProperty]}
+            onClick={() => onItemSelect(item)}
+          >
+            {item[textProperty]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ListGroup;
